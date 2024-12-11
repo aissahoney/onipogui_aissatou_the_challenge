54f796201e19207c85e9f9f7fa8d554e567ec026
aissahoney <aissatou.onipogui@gmail.com>
2024-12-11 23:18:56 +0100

    correction header display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { RouterProvider, createBrowserRouter ,Outlet, NavLink} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, Outlet, NavLink } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faSearch} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
@@ -13,9 +13,9 @@ import ErrorPage from './components/Error/Error';
 
 function App() {
   const [data, setData] = useState([])
-  const [mode, setMode]=useState(true)
- 
-  
+  const [mode, setMode] = useState(true)
+
+
   useEffect(() => {
     axios.get('https://restcountries.com/v2/all')
       .then((reponse) => setData(reponse.data))
@@ -24,14 +24,14 @@ function App() {
 
   const router = createBrowserRouter([
     {
-      path:'/',
+      path: '/',
       element: <Root />,
       errorElement: <ErrorPage />,
 
-      children: [ 
+      children: [
         {
-          path:'/',
-          element: <Countries data={data}/>
+          path: '/',
+          element: <Countries data={data} />
 
         }
         ,
@@ -39,37 +39,34 @@ function App() {
           path: '/:area',
           element: <CountryInfo data={data} />
         }
-      ]}
+      ]
+    }
   ])
 
- 
+
   function Root() {
     return (
 
       <div className="App">
-      {/* Message mobile */}
-      <div className="mobile-message">
-        <h2>Mobile version coming soon! Please visit us on a desktop.</h2>
+        {/* Message mobile */}
+        <div className="mobile-message">
+          <h2>Mobile version coming soon! Please visit us on a desktop.</h2>
+        </div>
+
+        {/* Contenu principal */}
+        <div className='desktop-content'>
+          <div className='header'>
+            <NavLink to='/'><h3>Where is The World </h3></NavLink>
+            {mode ? <h3 onClick={() => setMode(!mode)}> DarkMode</h3> :
+              <h3 onClick={() => setMode(!mode)}>LightMode</h3>}
+          </div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
       </div>
-
-      {/* Contenu principal */}
-      <div className='header desktop-content'>
-                <NavLink to='/'><h3>Where is The World </h3></NavLink> 
-                {mode ? <h3 onClick={()=>setMode(!mode)}> DarkMode</h3>:
-                <h3 onClick={()=>setMode(!mode)}>LightMode</h3>}
-                
-            </div>
-            <div>
-                <Outlet />
-            </div>
-    </div>
-     
-
     )
-}
-
-
-
+  }
   return (
 
     <div className={`${mode ? "lightMode" : "darkMode"}`}>
